57adbf024f17041d480ff2658b87ff5b803e221e
shadreza <shadreza100@gmail.com>
2023-04-10 00:09:05 +0600

    adding routes

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,32 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// adding new routes
+// '/hello' === 'hello'
+Route::get('/hello', function () {
+    return 'Hello World!';
+});
+
+// adding routes with response methods, status code and headers [html]
+Route::get('/response-html', function () {
+    return response('<h1>Hello World</h1>', 200)
+        ->header('Content-Type', 'text/html');
+});
+
+// adding routes with response methods, status code and headers [plain text]
+Route::get('/response-plain', function () {
+    return response('<h1>Hello World</h1>', 404)
+        ->header('Content-Type', 'text/text');
+});
+
+// wild card endpoint
+// adding routes with parameters
+Route::get('/wildcard/{id}', function ($id) {
+    return 'Wildcard no - ' . $id;
+});
+
+// wild card endpoint with conditions
+// adding regex will do
+Route::get('/wildcard/{id}', function ($id) {
+    return 'Wildcard no - ' . $id;
+})->where('id', '[0-9]+');
