9cb00bd117080ef11b7a20508e6d41109f23255d
shadreza <shadreza100@gmail.com>
2023-04-13 12:53:04 +0600

    if-else block

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,12 +1,10 @@
 <h1>{{ $heading }}</h1>
-{{-- add php directive --}}
-@php
-    $test = 1;
-@endphp
 
-{{ $test }}
-
-@foreach ($listings as $listing)
-    <h2>{{ $listing['title'] }}</h2>
-    <p>{{ $listing['description'] }}</p>
-@endforeach
+@if (count($listings) == 0)
+    <p>No listings found</p>
+@else
+    @foreach ($listings as $listing)
+        <h2>{{ $listing['title'] }}</h2>
+        <p>{{ $listing['description'] }}</p>
+    @endforeach
+@endif
