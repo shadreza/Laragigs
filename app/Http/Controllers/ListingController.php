94bd0834507845efa5ceca2429a26bd48ffb2d9d
shadreza <shadreza100@gmail.com>
2023-05-08 16:20:34 +0600

    pagination added

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -15,7 +15,7 @@ class ListingController extends Controller
         // dd(request()->tag);
 
         return view('listings.index', [
-            'listings' => Listing::latest()->filter(request(['tag', 'search']))->get()
+            'listings' => Listing::latest()->filter(request(['tag', 'search']))->paginate(6)
         ]);
     }
 
--- resources/views/listings/index.blade.php
+++ resources/views/listings/index.blade.php
@@ -15,4 +15,8 @@
 
     </div>
 
+    <div class="mt-6 p-4">
+        {{ $listings->links() }}
+    </div>
+
 </x-layout>
