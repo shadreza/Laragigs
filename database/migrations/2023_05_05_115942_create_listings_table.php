c6f8cf9c7f4eada78b3774f2e93833311e5b3ee5
shadreza <shadreza100@gmail.com>
2023-05-08 16:47:48 +0600

    logo field made unique

--- database/migrations/2023_05_05_115942_create_listings_table.php
+++ database/migrations/2023_05_05_115942_create_listings_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->string('email');
             $table->string('website');
             $table->longText('description');
-            $table->string('logo')->nullable();
+            $table->string('logo')->nullable()->unique();
             $table->timestamps();
         });
     }
